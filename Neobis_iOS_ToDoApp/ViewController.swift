65cc9a00c24c2372bf0ba998962c22c1936c2e75
aidar <sharipov_ai@auca.kg>
2023-04-18 15:13:43 +0600

    Initial Commit

---
+++ Neobis_iOS_ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_ToDoApp
+//
+//  Created by Айдар Шарипов on 18/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
